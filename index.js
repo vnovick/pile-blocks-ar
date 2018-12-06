41d00b60470db1dc20271f320d7b644db116a78e
vnovick <vnovick@gmail.com>
2018-12-06 22:47:12 +0200

    remove unneeded resources

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App.js';
 
-AppRegistry.registerComponent('ViroSample', () => App);
+AppRegistry.registerComponent('casualArGame', () => App);
 
 // The below line is necessary for use with the TestBed App
 AppRegistry.registerComponent('ViroSample', () => App);
